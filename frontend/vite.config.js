490509d20b63878761cec6de908cd4c9bf0b48ac
sujinbabups <sujinbabu85@gmail.com>
2025-01-07 12:13:18 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { baseUri } from './uri'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      [baseUri]: { 
-        target: 'http://localhost:5000',
+  server:{
+    proxy:{
+      '/api': {
+        target: `${import.meta.env.VITE_API}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    },
-  },
+
+    }
+  }
 })
